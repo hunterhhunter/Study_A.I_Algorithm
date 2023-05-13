f9c520cca28a1c2d1f9032a37a7200643c26826d
JoYoungJin <slugger613@naver.com>
2023-05-13 18:13:08 +0900

    23.05.13(토)

---
+++ Algorithm/Study_Algorithm/Stack&Deque/11866.py
@@ -0,0 +1,17 @@
+from collections import deque
+n, k = map(int, input().split())
+q = deque()
+result = []
+
+for i in range(1, n+1):
+    q.append(i)
+
+while q:
+    for i in range(k-1):
+        q.append(q.popleft())
+    result.append(q.popleft())
+
+print('<', end='')
+print(', '.join(map(str, result)), end='')
+print('>')
+
